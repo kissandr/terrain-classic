5c4d87f8e3660ef3775b695c0b10a54920b1512a
andrew <andrew@node4.idokep.hu>
2022-12-15 13:24:41 +0100

    wait for postgis db to spin up

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+cd /app/terrain-classic/
+./imposm import --cachedir cache -mapping /app/terrain-classic/imposm3_mapping.json -read /osm/europe-latest.osm.pbf -overwritecache
+./imposm import --cachedir cache -mapping /app/terrain-classic/imposm3_mapping.json -connection $DATABASE_URL -write
+./imposm import -mapping /app/terrain-classic/imposm3_mapping.json -connection $DATABASE_URL -optimize
+./imposm import -mapping /app/terrain-classic/imposm3_mapping.json -connection $DATABASE_URL -deployproduction
 make db/shared
 make
-/app/terrain-classic/imposm3 import --cachedir cache -mapping /app/terrain-classic/imposm3_mapping.json -read /osm/europe-latest.osm.pbf -overwritecache
-/app/terrain-classic/imposm3 import --cachedir cache -mapping /app/terrain-classic/imposm3_mapping.json -connection $DATABASE_URL -write
-/app/terrain-classic/imposm3 import -mapping /app/terrain-classic/imposm3_mapping.json -connection $DATABASE_URL -optimize
-/app/terrain-classic/imposm3 import -mapping /app/terrain-classic/imposm3_mapping.json -connection $DATABASE_URL -deployproduction
 
 
 
